9efd732dfeb9885cb8c64347aeab783190a1f4d8
Filip M <filip.moszczynski@onet.pl>
2020-10-31 19:30:28 +0100

    Initial commit

---
+++ src/main/java/com/filipmoszczynski/BestmixApplication.java
@@ -0,0 +1,13 @@
+package com.filipmoszczynski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestmixApplication.class, args);
+	}
+
+}
