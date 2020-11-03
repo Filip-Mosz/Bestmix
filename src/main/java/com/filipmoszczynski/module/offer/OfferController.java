4436c5579ba10f35c9a66753e4f15ae7e224f125
filip <filip.moszczynski@onet.pl>
2020-11-03 21:54:15 +0100

    OfferController.getProduct

--- src/main/java/com/filipmoszczynski/module/offer/OfferController.java
+++ src/main/java/com/filipmoszczynski/module/offer/OfferController.java
@@ -1,14 +1,47 @@
 package com.filipmoszczynski.module.offer;
 
+import com.filipmoszczynski.module.menu.MenuDto;
+import com.filipmoszczynski.module.menu.MenuService;
+import com.filipmoszczynski.module.offer.product.ProductDto;
+import com.filipmoszczynski.module.offer.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class OfferController {
 
+    @Autowired
+    public OfferController(MenuService menuService, ProductService productService) {
+        this.menuService = menuService;
+        this.productService = productService;
+    }
+
+    private final MenuService menuService;
+    private final ProductService productService;
+
     @RequestMapping("/offer")
     public String getOffer(){
         return "offer";
+    }
+
+    @RequestMapping("/offer/{id}")
+    public String getProduct(@PathVariable("id") String id, Model model) {
+        Optional<ProductDto> product = productService.getProduct(id);
+        List<MenuDto> menuItems = menuService.getMenu();
+        model.addAttribute("product", getProduct(product.get()));
+        model.addAttribute("menuItems", menuItems);
+
+        return "offer/product.html";
+    }
+
+    private ProductDto getProduct(ProductDto product) {
+        return product.setDescription(product.getDescription()); //not sure if how it should work...
     }
 }
 
