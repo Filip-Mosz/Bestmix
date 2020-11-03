939e7da30d46ddfc851e758718ea363ef286e662
filip <filip.moszczynski@onet.pl>
2020-11-03 21:20:34 +0100

    ProductService.getProducts

--- src/main/java/com/filipmoszczynski/module/offer/product/ProductService.java
+++ src/main/java/com/filipmoszczynski/module/offer/product/ProductService.java
@@ -1,4 +1,28 @@
 package com.filipmoszczynski.module.offer.product;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
+
+    public List<ProductDto> getProducts() {
+        try {
+            return  MAPPER.readValue(
+                    getClass().getClassLoader().getResource("products.json"),
+                    MAPPER.getTypeFactory().constructCollectionType(List.class, ProductDto.class)
+            );
+        } catch (IOException e) {
+            LOGGER.error("Error - No products found", e.getLocalizedMessage());
+            return List.of();
+        }
+    }
 }
