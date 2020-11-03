3f24753b1c64645da898cbb0d610ace15b2e7156
filip <filip.moszczynski@onet.pl>
2020-11-03 20:59:27 +0100

    ProductDto

--- src/main/java/com/filipmoszczynski/module/offer/OfferController.java
+++ src/main/java/com/filipmoszczynski/module/offer/OfferController.java
@@ -11,3 +11,5 @@ public class OfferController {
         return "offer";
     }
 }
+
+//demeter BlogController
--- src/main/java/com/filipmoszczynski/module/offer/product/ProductDto.java
+++ src/main/java/com/filipmoszczynski/module/offer/product/ProductDto.java
@@ -1,4 +1,35 @@
 package com.filipmoszczynski.module.offer.product;
 
 public class ProductDto {
+
+    private String id;
+    private String name;
+    private String header;
+
+    public String getId() {
+        return id;
+    }
+
+    public ProductDto setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ProductDto setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public ProductDto setHeader(String header) {
+        this.header = header;
+        return this;
+    }
 }
