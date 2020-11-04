1363c70e60c1695afcbfcac2b88dd69ae2fba570
filip <filip.moszczynski@onet.pl>
2020-11-04 19:39:22 +0100

    ProductServiceTest green

--- src/test/java/com/filipmoszczynski/module/offer/product/ProductServiceTest.java
+++ src/test/java/com/filipmoszczynski/module/offer/product/ProductServiceTest.java
@@ -1,16 +1,47 @@
 package com.filipmoszczynski.module.offer.product;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProductServiceTest {
 
+        ProductDto testProduct= new ProductDto();
+        ProductService testService = new ProductService();
+
+
+    @BeforeEach
+    void setUp() {
+
+        testProduct.setId("food");
+
+    }
+
     @Test
     void getProducts() {
+
+        assertEquals(testService.getProducts().size(), 4);
+
     }
 
     @Test
     void getProduct() {
+
+        ProductDto currentValue = extract(testService.getProduct("food"));
+
+        assertEquals(currentValue.getId(), testProduct.getId());
+
+    }
+
+    ProductDto extract (Optional<ProductDto> optional) {
+        if (optional.isPresent())
+        {
+            return optional.get();
+        }
+
+        return new ProductDto();
     }
 }
