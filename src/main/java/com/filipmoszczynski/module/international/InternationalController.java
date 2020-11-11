7df8459851524d834b893d592ea95a8393d0f020
filip <filip.moszczynski@onet.pl>
2020-11-11 10:43:43 +0100

    InternationalController.localeResolver

--- src/main/java/com/filipmoszczynski/module/international/InternationalController.java
+++ src/main/java/com/filipmoszczynski/module/international/InternationalController.java
@@ -1,4 +1,17 @@
 package com.filipmoszczynski.module.international;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
 public class InternationalController {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
 }
