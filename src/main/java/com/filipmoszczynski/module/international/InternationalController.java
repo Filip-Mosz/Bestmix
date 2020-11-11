9b8fa55a9dc1c6209ea612575cb7e42686ba0888
filip <filip.moszczynski@onet.pl>
2020-11-11 10:53:23 +0100

    localeChangeInterceptor & localeResolver moved to MvcConfig class

--- src/main/java/com/filipmoszczynski/module/international/InternationalController.java
+++ src/main/java/com/filipmoszczynski/module/international/InternationalController.java
@@ -1,27 +1,17 @@
 package com.filipmoszczynski.module.international;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+@Controller
 public class InternationalController {
 
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.US);
-        return slr;
-    }
 
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
-    }
 
 
 }
