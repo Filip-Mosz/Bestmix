6a3454f50b49440606dcfee6c280caae29b0b53b
filip <filip.moszczynski@onet.pl>
2020-11-11 11:48:50 +0100

    international subpage works

--- src/main/java/com/filipmoszczynski/module/international/InternationalController.java
+++ src/main/java/com/filipmoszczynski/module/international/InternationalController.java
@@ -1,13 +1,7 @@
 package com.filipmoszczynski.module.international;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Controller
 public class InternationalController {
